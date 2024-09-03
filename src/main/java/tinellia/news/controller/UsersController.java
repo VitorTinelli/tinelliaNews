40c9fcb9df82ce3e1259f062e66ee6db0b30517e
Vitor Tinelli <vitor.mtinelli@gmail.com>
2024-09-03 10:21:47 -0300

    Alpha 0.1.0f | fix: controller missing annotations

--- src/main/java/tinellia/news/controller/UsersController.java
+++ src/main/java/tinellia/news/controller/UsersController.java
@@ -1,5 +1,6 @@
 package tinellia.news.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import tinellia.news.domain.Users;
@@ -22,28 +23,28 @@ public class UsersController {
         return userService.getAllUsers();
     }
 
-    @GetMapping("/username")
-    public Users findUserByUsername(String username) {
+    @GetMapping("/find/{username}")
+    public Users findUserByUsername(@PathVariable String username) {
         return userService.findUserByUsername(username);
     }
 
     @GetMapping("/{id}")
-    public Users findUserByIdOrThrowException(UUID id) {
+    public Users findUserByIdOrThrowException(@PathVariable UUID id) {
         return userService.findUserByIdOrThrowException(id);
     }
 
     @PostMapping
-    public Users createUser(@RequestBody UserCreationRequest userCreationRequest) {
+    public Users createUser(@RequestBody @Valid UserCreationRequest userCreationRequest) {
         return userService.createUser(userCreationRequest.getUsername(), userCreationRequest.getPassword(), userCreationRequest.getEmail(), userCreationRequest.getAuthorities());
     }
 
     @PutMapping("/{id}")
-    public Users updateUser(@RequestBody UserUpdateRequest userUpdateRequest) {
+    public Users updateUser(@RequestBody @Valid UserUpdateRequest userUpdateRequest) {
         return userService.updateUser(userUpdateRequest.getId(), userUpdateRequest.getUsername(), userUpdateRequest.getPassword(), userUpdateRequest.getEmail(), userUpdateRequest.getAuthorities());
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUser(UUID id) {
+    public void deleteUser(@PathVariable UUID id) {
         userService.deleteUser(id);
     }
 }
